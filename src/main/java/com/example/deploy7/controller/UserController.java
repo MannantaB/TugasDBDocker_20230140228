b06f9380e4cda1f803994ca600593b6a3a31b026
MannantaB <mannanta18@gmail.com>
2026-04-20 10:34:44 +0700

    feat: add POST endpoint for creating users

--- src/main/java/com/example/deploy7/controller/UserController.java
+++ src/main/java/com/example/deploy7/controller/UserController.java
@@ -1,8 +1,13 @@
 package com.example.deploy7.controller;
 
+import com.example.deploy7.model.User;
 import com.example.deploy7.service.UserService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/users")
@@ -13,5 +18,9 @@ public class UserController {
         this.userService = userService;
     }
 
-
+    @PostMapping
+    public String createUser(@RequestBody User request) {
+        userService.addUser(request);
+        return "user created successfully";
+    }
 }
